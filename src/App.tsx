674beffe1a452c91a7118a95046f705f97c28d3b
Pablo Alan <pabloxt14@gmail.com>
2024-04-18 21:00:00 -0300

    fix: make some adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,30 @@ import { Icon360 } from './assets/icons/360'
 import { IconX } from './assets/icons/x'
 
 export function App() {
-  const [show360, setShow360] = useState(false)
+  const [show360, setShow360] = useState(true)
 
   function handleShow360() {
     setShow360((prevState) => !prevState)
   }
 
   return (
-    <main className="flex justify-center">
+    <main className="flex flex-col items-center justify-center gap-3 p-2 md:flex-row md:gap-0 md:p-0">
       <section className="relative">
-        <img src={show360 ? couchGif : bgCouch} alt="" className="w-[449px]" />
-        <button onClick={handleShow360} className="absolute right-12 top-2">
+        <img
+          src={show360 ? couchGif : bgCouch}
+          alt=""
+          onClick={handleShow360}
+          className="h-[222px] w-[449px] cursor-pointer object-cover"
+        />
+        <button
+          onClick={handleShow360}
+          className="absolute right-12 top-2"
+          aria-label={
+            show360
+              ? 'Esconder animação do sofa'
+              : 'Mostrar animação animação do sofa'
+          }
+        >
           {show360 ? <IconX /> : <Icon360 />}
         </button>
       </section>
@@ -27,10 +40,12 @@ export function App() {
           <h1 className="font-serif text-[32px] font-semibold">
             Sofá Margot II - Rosé
           </h1>
-          <strong className="text-base font-normal">R$ 4.000 </strong>
+          <strong className="text-base font-normal opacity-[0.67]">
+            R$ 4.000
+          </strong>
         </div>
 
-        <button className="border-secondary rounded-full border px-4 py-2 text-xs font-normal">
+        <button className="rounded-full border-[0.5px] border-secondary px-4 py-2 text-xs font-normal">
           ADICIONAR À CESTA
         </button>
       </section>
